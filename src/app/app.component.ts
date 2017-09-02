dbe797c45d63f709cc7db95355dda3ad8a37d5aa
Lucas Wiemers <lwiemers.0@gmail.com>
2017-09-02 14:05:21 +0200

    Need to add an session storage to determine if logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+  <!---wenn noch nicht eingeloggt-->
   <login></login>
+  <!--Wenn bereits eingeloggt zeige direkt den inhalt an-->
   <navbar></navbar>
   <content></content>
   `,
@@ -11,5 +13,5 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  
+
 }
--- src/app/contentholder.component.ts
+++ src/app/contentholder.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'content',
   template: `
-  <h1>Title</h1>
+  <h1>Page Title</h1>
+  Hier kommt dann der jeweilige inhalt rein
   Text ... blabla
    `
 })
