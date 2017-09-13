901b0ad031cbf72a4c0fedfb2491931ebfeea97d
Sebastian Kurz <sebastiankurz4@gmail.com>
2017-09-13 09:49:11 +0200

    * LoginService provides a method for getting teachers by their mail address * after the merge of the frontend and the backend google single sign in will be implemented in the new LoginService

---
+++ src/de/sklw/studentnote/login/LoginService.java
@@ -0,0 +1,42 @@
+package de.sklw.studentnote.login;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import de.sklw.studentnote.data.EMF;
+import de.sklw.studentnote.data.Teacher;
+
+@Path("/login/{mailAddress}")
+public class LoginService {
+	
+	private EntityManager em;
+	private List<Teacher> t_list;
+	private Teacher t;
+	
+	@SuppressWarnings("unchecked")
+	@GET
+	@Produces("application/json")
+	public String getTeacherByMailAddress(@PathParam("mailAddress") String mailAddress) {
+		
+		em = EMF.getEntityManager();
+		Query q = em.createQuery("SELECT t FROM Teacher t WHERE t.mailAddress = :mailAddress");
+		q.setParameter("mailAddress", mailAddress);
+		
+		this.t_list = (List<Teacher>)q.getResultList();
+		t_list = new ArrayList<>(t_list);
+		
+		if(t_list.size() > 0) {
+			t = t_list.get(0);
+			return t.toJSONString();
+		}
+		else return "{\"info\":\"no entity with this mail address found in storage\"}";
+	}
+
+}
